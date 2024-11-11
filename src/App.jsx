de559b02a6e5c5ee3fed6eba2f1fe30e7be2ffb0
fawzybas <fawzy.basuony@depowebeg.com>
2024-11-11 17:43:49 +0200

    form

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+import Confirmation from './components/confirmation/Confirmation'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Confirmation/>
+    </>
+  )
+}
+
+export default App
